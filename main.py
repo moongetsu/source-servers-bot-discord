93710734401d211626c3f2c653846add15bd0480
Moongetsu <111883135+moongetsu@users.noreply.github.com>
2022-12-24 01:11:29 +0200

    Changing Status & Commands
    Now if you have more than 1 server in the servers.cfg, the bot will change his status and it will shows the servers players and the current map.
    The added commands are:
    help
    servers - Send in an embed message the servers from servers.cfg. 
    serverinfo - Sends info about a server from the servers list.

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #   MIT License
 #   
 #   Copyright (c) 2022 - Stan '(xSLOW)' Valentin-Alexandru 
-#   
+#   Copyright (c) 2022 - Stan '(moongetsu)' 'Mario-Daniel'
+
 #   Permission is hereby granted, free of charge, to any person obtaining a copy
 #   of this software and associated documentation files (the "Software"), to deal
 #   in the Software without restriction, including without limitation the rights
@@ -32,48 +33,94 @@
 #    \______/  \______/  \______/ \__|       \_______| \_______|       \______/  \_______|\__|         \_/     \_______|\__|      \_______/       \__|     \__| \______/ \__|  \__|\__|   \____/  \______/ \__|      
 #                                                                                                                                                                                                                    
 #                                                                                                                                                                                                                     
-#   https://github.com/xSL0W?
+#   https://github.com/xSL0W? & https://github.com/moongetsu/
 
 # libraries
 import discord
 from discord.ext import tasks
 from discord.errors import InvalidArgument
-
+from discord.ext import commands
 import a2s
 import socket
 import asyncio
 import os
+import base64
 import json
+import colorama
+from colorama import Fore, Back, Style
+import platform
+import sys
+import os
 
-# Main Code - Get settings
+# Main Settings
 with open('settings.json') as json_file:
     data = json.load(json_file)
     for p in data['settings']:
         global REFRESH_TIME, TOKEN
         REFRESH_TIME = p['refresh-time']
         TOKEN = p['bot-token']
+        COMMUNITY_NAME = p['community-name']
+        PREFIX = p['prefix']
 
+# Prefix & Intents
+intents = discord.Intents.all()
+bot = commands.Bot(f"{PREFIX}", intents=intents)
+
+bot.remove_command('help')
 
 # dont change
-VERSION = "0.1b"
+VERSION = "1.0b"
 
-# Global Variables
-bot = discord.Client()
-
-
-# On bot ready event. Called when the bot is loaded.
 @bot.event
 async def on_ready():
     await bot.wait_until_ready()
-    print('Im online :)')
+    print('------------------------------------------------------------------------------------------------------------------------------------------')
+    print(Fore.YELLOW + '███╗   ███╗ ██████╗  ██████╗ ███╗   ██╗ ██████╗ ███████╗███████╗██╗   ██╗    ███████╗██╗   ██╗███████╗████████╗███████╗███╗   ███╗███████╗')
+    print(Fore.YELLOW + '████╗ ████║██╔═══██╗██╔═══██╗████╗  ██║██╔════╝ ██╔════╝██╔════╝██║   ██║    ██╔════╝╚██╗ ██╔╝██╔════╝╚══██╔══╝██╔════╝████╗ ████║██╔════╝')
+    print(Fore.YELLOW + '██╔████╔██║██║   ██║██║   ██║██╔██╗ ██║██║  ███╗█████╗  ███████╗██║   ██║    ███████╗ ╚████╔╝ ███████╗   ██║   █████╗  ██╔████╔██║███████╗')
+    print(Fore.YELLOW + '██║╚██╔╝██║██║   ██║██║   ██║██║╚██╗██║██║   ██║██╔══╝  ╚════██║██║   ██║    ╚════██║  ╚██╔╝  ╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║╚════██║')
+    print(Fore.YELLOW + '██║ ╚═╝ ██║╚██████╔╝╚██████╔╝██║ ╚████║╚██████╔╝███████╗███████║╚██████╔╝    ███████║   ██║   ███████║   ██║   ███████╗██║ ╚═╝ ██║███████')
+    print(Fore.YELLOW + '╚═╝     ╚═╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚═════╝ ╚══════╝╚══════╝ ╚═════╝     ╚══════╝   ╚═╝   ╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝')
+    print('------------------------------------------------------------------------------------------------------------------------------------------')
+    print(f'The host has been connected to {bot.user.name}')
+    print(f'The ID of the bot is: {bot.user.id}')
+    print(f"Python version: {platform.python_version()}")
+    print(f"Discord.py version: {discord.__version__}")
+    print(f"The bot runs on: {platform.system()} {platform.release()} ({os.name})")
+    print(f"------------------------------------------------------------------------------------------------------------------------------------------")
     loop_renew_status.start()
 
-# This will refresh the bot's status every REFRESH_TIME seconds
 @tasks.loop(seconds=REFRESH_TIME)
 async def loop_renew_status():
-    await SetDiscordStatus(GenerateStatus())
+    servers = {}
 
-# Set a status on discord
+    with open("servers.cfg", "r") as f:
+        for line in f:
+            line = line.rstrip()
+            list = line.split(":", 2)
+            print("IP/DNS: " + list[0] + " PORT: " + list[1])
+            print("")
+            
+            address = (list[0], int(list[1]))
+            try:
+                csquery = a2s.info(address)
+                servers[list[0]] = {
+                    "playercount": csquery.player_count,
+                    "maxslots": csquery.max_players,
+                    "map": csquery.map_name
+                }
+            except (socket.gaierror, a2s.BrokenMessageError, a2s.BufferExhaustedError, asyncio.exceptions.TimeoutError, socket.timeout, ConnectionRefusedError, OSError)  as e:
+                print("### (WARNING) Exception occured while trying to query " + line + " - Please check for problems")
+                continue   
+
+        f.close()
+
+    for server_name, server_info in servers.items():
+        status = f"{server_info['playercount']}/{server_info['maxslots']} on {server_info['map']}"
+        await SetDiscordStatus(status)
+        
+        await asyncio.sleep(REFRESH_TIME)
+
 async def SetDiscordStatus(status = ""):
     activity = discord.Game(name=status, type=3)
     try:
@@ -81,10 +128,8 @@ async def SetDiscordStatus(status = ""):
     except InvalidArgument:
         print("Exception: Invalid argument")
         pass
-        
-# Generate status by query-ing servers
+
 def GenerateStatus():
-    # initialize variables
     playercount = 0
     maxslots = 0
     count = 0
@@ -93,8 +138,8 @@ def GenerateStatus():
     # read file
     with open("servers.cfg", "r") as f:
         for line in f:
-            line = line.rstrip() # remove whitespaces
-            list = line.split(":", 2) # split text in 2 elements
+            line = line.rstrip()
+            list = line.split(":", 2)
             print("IP/DNS: " + list[0] + " PORT: " + list[1])
             
             # query servers
@@ -106,28 +151,107 @@ def GenerateStatus():
                 print("### (WARNING) Exception occured while trying to query " + line + " - Please check for problems")
                 continue   
 
-            # increase playercount/maxslots
             playercount += csquery.player_count
             maxslots += csquery.max_players
 
-            # get map name
             map = csquery.map_name
 
-            # increase server count
             count = count + 1
 
-        f.close() # close the file after using
+        f.close()
 
 
-    if count == 0: # Case if all servers are offline
+    if count == 0:
         status = "Server(s) Offline"
     elif count == 1:
-        status = str(playercount) + "/" + str(maxslots) + " on " + map
-    elif count > 1: # Case if there are multiple servers
-        status = str(playercount) + "/" + str(maxslots) + " on " + str(count) + " servers"
+        status = str(name) + str(playercount) + "/" + str(maxslots) + " on " + map
+    elif count > 1:
+        status = str(name) + str(playercount) + "/" + str(maxslots) + " on " + str(count) + " servers"
 
     print(status)
     return (status)
 
+#----------------------------------------Commands-----------------------------------------
+# Servers
+@bot.command(name='servers', help='Displays the IP addresses and server names of the servers in servers.cfg')
+async def servers(ctx):
+    with open("servers.cfg", "r") as f:
+        servers = []
+        for line in f:
+            if line.startswith("//"):
+                continue
+            try:
+                ip, port, name = line.split(":")
+            except ValueError:
+                ip, port = line.split(":")
+                name = "Unnamed server"
+            servers.append((ip, port, name.rstrip()))
+    
+    embed = discord.Embed(title=f"{COMMUNITY_NAME}'s Servers", color=0x000000)
+
+    guild = ctx.guild
+
+    embed.set_author(name=guild.name, icon_url=guild.icon_url)
+    embed.set_footer(text=f"Use {PREFIX}serverinfo to see the info about a server")
+    for i, (ip, port, name) in enumerate(servers):
+        address = (ip, int(port))
+        try:
+            csquery = a2s.info(address)
+            player_count = csquery.player_count
+            player_max = csquery.max_players
+        except (socket.gaierror, a2s.BrokenMessageError, a2s.BufferExhaustedError, asyncio.exceptions.TimeoutError, socket.timeout, ConnectionRefusedError, OSError) as e:
+            player_count = "Error querying server"
+            player_max = "Error querying server"
+        embed.add_field(name=f"`{name}`", value=f"IP/DNS: **{ip}:{port}** (*{player_count}/{player_max}*)", inline=False)
+    await ctx.send(embed=embed)
+
+
+# Server Info
+@bot.command()
+async def serverinfo(ctx, server_ip: str, server_port: int = 27015):
+    found = False
+    with open("servers.cfg", "r") as f:
+        for line in f:
+            line = line.rstrip()
+            ip, port, *_ = line.split(":")
+            if ip == server_ip and port == str(server_port):
+                found = True
+                break
+    if not found:
+        embed = discord.Embed(title="Error 👀", description=f"`{server_ip}` cannot be found in `servers.cfg` file.", color=0xff0000)
+        embed.set_footer(text="© Moongetsu Systems™ (2020-2022) | Source Servers Bot (v2 beta)")
+        await ctx.reply(embed=embed, mention_author=False)
+        return
+    address = (server_ip, server_port)
+    try:
+        csquery = a2s.info(address)
+    except (socket.gaierror, a2s.BrokenMessageError, a2s.BufferExhaustedError, asyncio.exceptions.TimeoutError, socket.timeout, ConnectionRefusedError, OSError) as e:
+        await ctx.reply("Error querying server: {}".format(e))
+        return
+
+    guild = ctx.guild
+    
+    banner_url = f"https://cache.gametracker.com/server_info/{server_ip}:{server_port}/b_560_95_1.png"
+    embed = discord.Embed(title=csquery.server_name, description=f"steam://connect/{server_ip}:{port}", color=0x00000)
+    embed.set_author(name=guild.name, icon_url=guild.icon_url)
+    embed.set_image(url=banner_url)
+    embed.add_field(name="IP/DNS 🌍", value="```{}```".format(server_ip))
+    embed.add_field(name="Current Map 🗺️", value=f"```{csquery.map_name}```")
+    embed.add_field(name="Current players 🛡️", value="```{}/{}```".format(csquery.player_count, csquery.max_players))
+    embed.set_footer(text="© Moongetsu Systems™ (2020-2022) | Source Servers Bot (v1.0b)")
+
+    await ctx.reply(embed=embed, mention_author=False)
+
+# Help
+@bot.command()
+async def help(ctx):
+    guild = ctx.guild
+
+    embed = discord.Embed(title=f"Source Servers Bot Commands", color=0x000000)
+    embed.set_author(name=guild.name, icon_url=guild.icon_url)
+    embed.add_field(name=":page_with_curl: | servers", value="See the servers from our community.", inline=False)
+    embed.add_field(name=":chart_with_upwards_trend: | serverinfo `<serverip>`", value="Sends info about a server from the servers list.", inline=False)
+    embed.set_footer(text="© Moongetsu Systems™ (2020-2022) | Source Servers Bot (v1.0b)")
+    await ctx.send(embed=embed)
 
 bot.run(TOKEN)
